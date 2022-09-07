9fde106999ad5082c6ae618d0e8dd139156808e9
Савенчук Любовь <sav_lu@gmail.com>
2022-09-07 22:06:39 +0500

    ex3/

---
+++ ex3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int N = 15;
+int[] array = new int[N];
+int index = 0;
+int max = array[0];
+int min = array[0];
+
+while (index < N)
+{
+   array[index] = new Random().Next(0, 100);
+   Console.Write(array[index] + " ");
+   index++;
+}
+
+for(index = 1; index < array.Length; index++)
+{
+   if(array[index] < min)
+   {
+      min = array[index];
+   }
+   if(array[index] > max)
+   {
+      max = array[index];
+   }
+}
+
+Console.WriteLine();
+Console.WriteLine("max = " + max);
+Console.WriteLine("min = " + min);
