e4927c3bddaf3879da8cf137f4edcce49bc69211
Савенчук Любовь <sav_lu@gmail.com>
2022-09-09 22:20:45 +0500

    ex2/

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,6 +1,4 @@
 ﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму четных элементов, стоящих на нечётных позициях (позиция - индекс элемента в массиве).
-//[3, 7, 23, 12] -> 12
-//[4, 6, 8, 1, 4] -> 6
 
 // создание массива
 int[] number = new int[2];
@@ -12,22 +10,19 @@ for(int i = 0; i < number.Length; i++)
 {
    number[i] = rnd.Next(1, 500);
    Console.Write(number[i] + " ");
-   
-   // поиск нечетных индексов
-   for(index = 0; index % 2 != 0; index++)
-   {
-      if(number[index] % 2 == 0)
-      {
-         sum = sum + number[index];
-         
-      }
-      else
-      {
-         index++;
-      }
-   }
 
 } 
-   
+// поиск нечетных индексов
+for(index = 1; index < number.Length; index+=2)
+{
+   if(number[index] % 2 == 0)
+   {
+      sum = sum + number[index];
+   }
+   else
+   {
+      index++;
+   }
+}
 Console.WriteLine();
 Console.WriteLine("Сумма четных элементов составляет " + sum);
